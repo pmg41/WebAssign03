520888a326c76e518e05b2e6df10339afc131ed4
pragnesh30gajera <pragnesh30gajera@gmail.,com>
2024-03-28 19:20:06 -0400

    -CRUD is done in API for Comments

--- Controllers/CommentsController.cs
+++ Controllers/CommentsController.cs
@@ -9,6 +9,8 @@ using Assign03.Data;
 
 namespace Assign03.Controllers
 {
+    [Route("[controller]")]
+    [ApiController]
     public class CommentsController : Controller
     {
         private readonly WebAssign3Context _context;
@@ -19,133 +21,83 @@ namespace Assign03.Controllers
         }
 
         // GET: Comments
-        public async Task<IActionResult> Index()
+        [HttpGet]
+        public async Task<ActionResult<IEnumerable<Comment>>> GetComments()
         {
-            return View(await _context.Comments.ToListAsync());
+            return await _context.Comments.ToListAsync();
         }
 
-        // GET: Comments/Details/5
-        public async Task<IActionResult> Details(int? id)
+        // GET: Comments/5
+        [HttpGet("{id}")]
+        public async Task<ActionResult<Comment>> GetCommentById(int? id)
         {
-            if (id == null)
-            {
-                return NotFound();
-            }
-
-            var comment = await _context.Comments
-                .FirstOrDefaultAsync(m => m.CommentId == id);
-            if (comment == null)
-            {
-                return NotFound();
-            }
-
-            return View(comment);
-        }
-
-        // GET: Comments/Create
-        public IActionResult Create()
-        {
-            return View();
-        }
-
-        // POST: Comments/Create
-        // To protect from overposting attacks, enable the specific properties you want to bind to.
-        // For more details, see http://go.microsoft.com/fwlink/?LinkId=317598.
-        [HttpPost]
-        [ValidateAntiForgeryToken]
-        public async Task<IActionResult> Create([Bind("CommentId,ProductId,UserId,Rating,Images,Text")] Comment comment)
-        {
-            if (ModelState.IsValid)
-            {
-                _context.Add(comment);
-                await _context.SaveChangesAsync();
-                return RedirectToAction(nameof(Index));
-            }
-            return View(comment);
-        }
-
-        // GET: Comments/Edit/5
-        public async Task<IActionResult> Edit(int? id)
-        {
-            if (id == null)
-            {
-                return NotFound();
-            }
-
             var comment = await _context.Comments.FindAsync(id);
+
             if (comment == null)
             {
                 return NotFound();
             }
-            return View(comment);
+
+            return comment;
         }
 
-        // POST: Comments/Edit/5
+        // POST: Comments/
+        [HttpPost]
+        public async Task<ActionResult<Comment>> AddItemInComment(Comment comment)
+        {
+            if (!ModelState.IsValid)
+            {
+                return BadRequest(ModelState);
+            }
+
+            _context.Comments.Add(comment);
+            await _context.SaveChangesAsync();
+
+            return CreatedAtAction(nameof(GetCommentById), new { id = comment.CommentId }, new { status = 201, message = "Comment saved successfully" });
+        }
+
+
+        // PUT: Comments/5
         // To protect from overposting attacks, enable the specific properties you want to bind to.
         // For more details, see http://go.microsoft.com/fwlink/?LinkId=317598.
-        [HttpPost]
-        [ValidateAntiForgeryToken]
-        public async Task<IActionResult> Edit(int id, [Bind("CommentId,ProductId,UserId,Rating,Images,Text")] Comment comment)
+        [HttpPut("{id}")]
+        public async Task<IActionResult> EditCommentById(int id, [Bind("CommentId,ProductId,UserId,Rating,Images,Text")] Comment comment)
         {
             if (id != comment.CommentId)
             {
-                return NotFound();
+                return BadRequest(new { status = 400, message = "Comment ID mismatch" });
             }
 
             if (ModelState.IsValid)
             {
-                try
+                if (!CommentExists(comment.CommentId))
                 {
-                    _context.Update(comment);
-                    await _context.SaveChangesAsync();
+                    return NotFound(new { status = 404, message = "Comment Not Found" });
                 }
-                catch (DbUpdateConcurrencyException)
-                {
-                    if (!CommentExists(comment.CommentId))
-                    {
-                        return NotFound();
-                    }
-                    else
-                    {
-                        throw;
-                    }
-                }
-                return RedirectToAction(nameof(Index));
+                _context.Update(comment);
+                await _context.SaveChangesAsync();
             }
-            return View(comment);
+            return Ok(new { status = 200, message = "Comment updated successfully" }); ;
         }
 
-        // GET: Comments/Delete/5
-        public async Task<IActionResult> Delete(int? id)
+
+        // DELETE: Comments/5
+        [HttpDelete("{id}")]
+        public async Task<IActionResult> DeleteItemFromCommentById(int id)
         {
-            if (id == null)
+            if (!CommentExists(id))
             {
-                return NotFound();
+                return NotFound(new { status = 404, message = "Comment Not Found" });
             }
-
-            var comment = await _context.Comments
-                .FirstOrDefaultAsync(m => m.CommentId == id);
-            if (comment == null)
-            {
-                return NotFound();
-            }
-
-            return View(comment);
-        }
-
-        // POST: Comments/Delete/5
-        [HttpPost, ActionName("Delete")]
-        [ValidateAntiForgeryToken]
-        public async Task<IActionResult> DeleteConfirmed(int id)
-        {
             var comment = await _context.Comments.FindAsync(id);
+
             if (comment != null)
             {
                 _context.Comments.Remove(comment);
+                await _context.SaveChangesAsync();
             }
 
-            await _context.SaveChangesAsync();
-            return RedirectToAction(nameof(Index));
+            return Ok(new { status = 200, message = "Comment deleted successfully" }); ;
         }
 
         private bool CommentExists(int id)
